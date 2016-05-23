dcb5ae6734b37e831298fbfdffae794e8695c292
Jayone Lai <lz881228@hotmail.com>
2016-05-23 16:05:49 +0800

    更新
    数据库

--- src/main/resources/article_db.sql
+++ src/main/resources/article_db.sql
@@ -36,35 +36,41 @@ CREATE TABLE IF NOT EXISTS `article` (
 -- 导出  表 article.crawler 结构
 DROP TABLE IF EXISTS `crawler`;
 CREATE TABLE IF NOT EXISTS `crawler` (
-  `id` tinyint(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
+  `id` varchar(32) NOT NULL,
+  `name` varchar(50) DEFAULT NULL COMMENT '某某产品',
+  `scope` varchar(100) DEFAULT NULL COMMENT '指定域名范围',
   `start_url` varchar(200) DEFAULT NULL COMMENT '初始URL',
   `start_url_template` varchar(200) DEFAULT NULL COMMENT '开始URL模板（正则表达式）',
   `target_url_template` varchar(200) DEFAULT NULL COMMENT '目标URL模板（正则表达式）',
+  `content_grab_expression` varchar(100) DEFAULT NULL COMMENT '目标内容提取表达式',
+  `author_grab_expression` varchar(100) DEFAULT NULL COMMENT '目标内容:作者提取表达式',
+  `date_grab_expression` varchar(100) DEFAULT NULL COMMENT '目标内容:日期提取表达式',
+  `starts_grab_expression` varchar(100) DEFAULT NULL COMMENT '目标内容:星星提取表达式',
+  `status` int(11) DEFAULT '0' COMMENT '1:开启，0:停用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='网络爬虫配置信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='网络爬虫配置信息表';
 
--- 正在导出表  article.crawler 的数据：~1 rows (大约)
+-- 正在导出表  article.crawler 的数据：~18 rows (大约)
 /*!40000 ALTER TABLE `crawler` DISABLE KEYS */;
-INSERT INTO `crawler` (`id`, `name`, `start_url`, `start_url_template`, `target_url_template`) VALUES
-	(1, '1', '1', '1', '1'),
-	(2, '2', '2', '2', '2'),
-	(3, '3', '3', '3', '3'),
-	(4, '4', '4', '4', '4'),
-	(5, '5', '5', '5', '5'),
-	(6, '6', '6', '6', '6'),
-	(7, '7', '7', '7', '7'),
-	(8, '8', '8', '8', '8'),
-	(9, '9', '9', '9', '9'),
-	(10, '10', '10', '10', '10'),
-	(11, '11', '11', '11', '11'),
-	(12, '12', '12', '12', '12'),
-	(13, '13', '13', '13', '13'),
-	(14, '14', '14', '14', '14'),
-	(15, '15', '15', '15', '15'),
-	(16, '16', '16', '16', '16'),
-	(17, '17', '17', '17', '17'),
-	(18, '18', '18', '18', '18');
+INSERT INTO `crawler` (`id`, `name`, `scope`, `start_url`, `start_url_template`, `target_url_template`, `content_grab_expression`, `author_grab_expression`, `date_grab_expression`, `starts_grab_expression`, `status`) VALUES
+	('10', '10', NULL, '10', '10', '10', NULL, NULL, NULL, NULL, 0),
+	('11', '11', NULL, '11', '11', '11', NULL, NULL, NULL, NULL, 0),
+	('11111111111111111111111111111111', '亚马逊产品评论爬虫', 'www.amazon.com', 'http://www.amazon.com/Ooma-Telo-Free-Phone-Service/product-reviews/B00I4XMEYA/ref=cm_cr_getr_d_paging_btm_1?ie=UTF8&showViewpoints=1&sortBy=helpful&pageNumber=1', '', '//ul[@class=\'a-pagination\']', '//span[@class=\'a-size-base review-text\']/text()', '//a[@class=\'a-size-base a-link-normal author\']/text()', '//span[@class=\'a-size-base a-color-secondary review-date\']/text()', '//span[@class=\'a-icon-alt\']/text()', 1),
+	('12', '12', NULL, '12', '12', '12', NULL, NULL, NULL, NULL, 0),
+	('13', '13', NULL, '13', '13', '13', NULL, NULL, NULL, NULL, 0),
+	('14', '14', NULL, '14', '14', '14', NULL, NULL, NULL, NULL, 0),
+	('15', '15', NULL, '15', '15', '15', NULL, NULL, NULL, NULL, 0),
+	('16', '16', NULL, '16', '16', '16', NULL, NULL, NULL, NULL, 0),
+	('17', '17', NULL, '17', '17', '17', NULL, NULL, NULL, NULL, 0),
+	('18', '18', NULL, '18', '18', '18', NULL, NULL, NULL, NULL, 0),
+	('2', '2', NULL, '2', '2', '2', NULL, NULL, NULL, NULL, 0),
+	('3', '3', NULL, '3', '3', '3', NULL, NULL, NULL, NULL, 0),
+	('4', '4', NULL, '4', '4', '4', NULL, NULL, NULL, NULL, 0),
+	('5', '5', NULL, '5', '5', '5', NULL, NULL, NULL, NULL, 0),
+	('6', '6', NULL, '6', '6', '6', NULL, NULL, NULL, NULL, 0),
+	('7', '7', NULL, '7', '7', '7', NULL, NULL, NULL, NULL, 0),
+	('8', '8', NULL, '8', '8', '8', NULL, NULL, NULL, NULL, 0),
+	('9', '9', NULL, '9', '9', '9', NULL, NULL, NULL, NULL, 0);
 /*!40000 ALTER TABLE `crawler` ENABLE KEYS */;
 
 
@@ -88,16 +94,13 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   `id` varchar(32) NOT NULL,
   `review` varchar(10000) DEFAULT NULL,
   `reviewer` varchar(100) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `date` date NOT NULL DEFAULT '0000-00-00',
   `helpful_count` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `starts` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `fulltext_reviews` (`review`,`reviewer`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- 正在导出表  article.reviews 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
-INSERT INTO `reviews` (`id`, `review`, `reviewer`, `date`, `helpful_count`) VALUES
-	('1', '今天天气很好', '啊次', '2016-05-16 17:05:56', 1);
-/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
 
 
 -- 导出  表 article.url 结构
