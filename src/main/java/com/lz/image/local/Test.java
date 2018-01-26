160c19f0bd49d3f37aa9133132ee795d466d7652
laizhiwen <laizhiwen@gionee.com>
2018-01-26 09:30:23 +0800

    本地模糊图片算法

---
+++ src/main/java/com/lz/image/local/Test.java
@@ -0,0 +1,36 @@
+package com.lz.image.local;
+import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * @author 444716720
+ * 高斯模糊
+ *
+ */
+
+public class Test {
+	public static void main(String[] args) {
+		try {
+			//读取原图
+			File file = new File("C:\\Users\\gionee\\Desktop\\dog.jpg");
+			BufferedImage b1 = ImageIO.read(file);
+			//模糊
+			GaussianFilter filter = new GaussianFilter(35);
+			BufferedImage blurredImage = filter.filter(b1, new BufferedImage(b1.getWidth(), b1.getHeight(), BufferedImage.TYPE_INT_ARGB));
+			//保存处理后的图
+			File newFile = new File("C:\\Users\\gionee\\Desktop\\dog-blur.jpg");
+			ImageIO.write(blurredImage, "jpg", newFile);
+			
+			//打开文件
+			if (Desktop.isDesktopSupported()
+					&& Desktop.getDesktop().isSupported(Desktop.Action.OPEN))
+				Desktop.getDesktop().open(newFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
