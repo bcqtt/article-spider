891e82a6c34e5cac307b6b06787622e1e7dd8981
laizhiwen <laizhiwen@evergrande.com>
2019-02-15 17:38:15 +0800

    rabbitmq test
    rabbitmq test

--- src/main/java/com/lz/jms/rabbitmq/Recv.java
+++ src/main/java/com/lz/jms/rabbitmq/Recv.java
@@ -10,30 +10,29 @@ import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.ShutdownSignalException;
 
 public class Recv {
-	//队列名称  
-    private final static String QUEUE_NAME = "hello"; 
-    
-    public static void main(String[] args) throws ShutdownSignalException, 
-    	ConsumerCancelledException, 
-    	InterruptedException, IOException {
-    	//打开连接和创建频道，与发送端一样  
-        ConnectionFactory factory = new ConnectionFactory();  
-        factory.setHost("localhost");  
-        Connection connection = factory.newConnection();  
-        Channel channel = connection.createChannel();  
-        //声明队列，主要为了防止消息接收者先运行此程序，队列还不存在时创建队列。  
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");  
-          
-        //创建队列消费者  
-        QueueingConsumer consumer = new QueueingConsumer(channel);  
-        //指定消费队列  
-        channel.basicConsume(QUEUE_NAME, true, consumer);  
-        while (true){  
-            //nextDelivery是一个阻塞方法（内部实现其实是阻塞队列的take方法）  
-            QueueingConsumer.Delivery delivery = consumer.nextDelivery();  
-            String message = new String(delivery.getBody());  
-            System.out.println(" [x] Received '" + message + "'");  
-        }  
+	// 队列名称
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args)
+			throws ShutdownSignalException, ConsumerCancelledException, InterruptedException, IOException {
+		// 打开连接和创建频道，与发送端一样
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		// 声明队列，主要为了防止消息接收者先运行此程序，队列还不存在时创建队列。
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		// 创建队列消费者
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		// 指定消费队列
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+		while (true) {
+			// nextDelivery是一个阻塞方法（内部实现其实是阻塞队列的take方法）
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
 	}
 }
--- src/main/java/com/lz/jms/rabbitmq/Send.java
+++ src/main/java/com/lz/jms/rabbitmq/Send.java
@@ -8,30 +8,30 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Send {
-	
-	//队列名称  
-    private final static String QUEUE_NAME = "hello";  
 
-    public static void main(String[] args) throws IOException, TimeoutException {
-    	/** 
-         * 创建连接连接到MabbitMQ 
-         */  
-        ConnectionFactory factory = new ConnectionFactory();  
-        //设置MabbitMQ所在主机ip或者主机名  
-        factory.setHost("localhost");  
-        //创建一个连接  
-        Connection connection = factory.newConnection();  
-        //创建一个频道  
-        Channel channel = connection.createChannel();  
-        //指定一个队列  
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
-        //发送的消息  
-        String message = "hello world!";  
-        //往队列中发出一条消息  
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());  
-        System.out.println(" [x] Sent '" + message + "'");  
-        //关闭频道和连接  
-        channel.close();  
-        connection.close();  
+	// 队列名称
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		/**
+		 * 创建连接连接到MabbitMQ
+		 */
+		ConnectionFactory factory = new ConnectionFactory();
+		// 设置MabbitMQ所在主机ip或者主机名
+		factory.setHost("localhost");
+		// 创建一个连接
+		Connection connection = factory.newConnection();
+		// 创建一个频道
+		Channel channel = connection.createChannel();
+		// 指定一个队列
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		// 发送的消息
+		String message = "hello world!";
+		// 往队列中发出一条消息
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		// 关闭频道和连接
+		channel.close();
+		connection.close();
 	}
 }
