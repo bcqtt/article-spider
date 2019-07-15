de9ce1b05039a97a93a92f216394c608f4c39791
laizhiwen <laizhiwen@evergrande.com>
2019-07-15 15:17:16 +0800

    1

--- src/main/java/com/lz/jms/rabbitmq/fanout/Constants.java
+++ src/main/java/com/lz/jms/rabbitmq/fanout/Constants.java
@@ -2,17 +2,17 @@ package com.lz.jms.rabbitmq.fanout;
 public class Constants {
 
 
-    public static String direct_exchange = "dy-exchange";
-    public  static String queue_direct = "dy-queue";
+    public static String direct_exchange = "lzw-exchange";
+    public  static String queue_direct = "lzw-queue";
 
-    public static  String fanout_exchange = "dy-exchange-fanout";
-    public static  String queue_fanout_1 = "dy-queue-fanout-1";
-    public static  String queue_fanout_2 = "dy-queue-fanout-2";
+    public static  String fanout_exchange = "lzw-exchange-fanout";
+    public static  String queue_fanout_1 = "lzw-queue-fanout-1";
+    public static  String queue_fanout_2 = "lzw-queue-fanout-2";
 
 
-    public static String topic_exchange = "dy-exchange-topic";
-    public static String queue_topic_1 = "dy-queue-topic-1";
-    public static String queue_topic_2 = "dy-queue-topic-2";
-    public static String queue_topic_3 = "dy-queue-topic-3";
+    public static String topic_exchange = "lzw-exchange-topic";
+    public static String queue_topic_1 = "lzw-queue-topic-1";
+    public static String queue_topic_2 = "lzw-queue-topic-2";
+    public static String queue_topic_3 = "lzw-queue-topic-3";
 
 }
